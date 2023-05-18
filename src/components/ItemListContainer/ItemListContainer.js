faedb49e9b56b1989a330c8b114a386a03cc5510
Nicolás Godoy <nicolasgastongodoy@gmail.com>
2023-05-18 20:56:30 -0300

    cambio

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -3,7 +3,7 @@ import React from "react";
 const ItemListContainer = ({ greeting }) => {
   return (
     <div className="w-75 ">
-      <h2 class="gradiente-morado w-75 px-2 py-2 bg-opacity-10">{greeting}<i class="bi bi-phone-flip fs-1 mb-4 mx-2 align-items-center"></i></h2>
+      <h2 class="gradiente-morado w-75 px-2 py-2">{greeting}<i class="bi bi-phone-flip fs-1 mb-4 mx-2 align-items-center"></i></h2>
     </div>
     
   );
